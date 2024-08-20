2621791800b0896091c7d2b635006539f3707e58
KangJEoungMi <kbg973855@gmail.com>
2024-08-20 16:19:42 +0900

    restart

---
+++ ReactMaster/react-master/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Circle from './Circle';
+// Props Types 는 prop이 거기에 있는지 없는지 확인해주지만, 코드를 실행 한 "후"에만 확인이 가능하다.
+// typeScript
+function App() {
+  return (
+    <div className="App">
+      <Circle bgColor="teal"/>
+      <Circle bgColor="tomato"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactMaster/react-master/src/Circle.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+interface ContainerProps{
+    bgColor: string ;
+}
+
+const Container = styled.div<ContainerProps>`
+    width: 200px;
+    height: 200px;
+    background-color: ${(props) => props.bgColor};
+    border-radius: 100px;
+`;
+
+// const x = (a:number, b:number) => a+b;
+interface CircleProps{
+    bgColor: string ;
+}
+
+
+function Circle({bgColor}:CircleProps){
+    return <Container bgColor={bgColor}/>;
+}
+
+export default Circle;
